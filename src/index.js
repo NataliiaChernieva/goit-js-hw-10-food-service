8a7791a18ae0f5f57f90774a17ceead3e657226b
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-04-28 12:24:36 +0300

    готовая

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
-import './style.css';
+import './styles.css';
 import menu from './menu.json';
 import menuItemsTemplate from './menu-items.hbs';
 
-const menuRef = document.querySelector('.js-menu');
+const menuEl = document.querySelector('.js-menu');
+const bodyEl = document.querySelector('body');
+const inputEl = document.querySelector('.theme-switch__toggle');
 
 const markup = menuItemsTemplate(menu);
-menuRef.insertAdjacentHTML('beforeend', markup);
+menuEl.insertAdjacentHTML('beforeend', markup);
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
-const inputEl = document.querySelector('#theme -switch-toggle');
+reload();
+//bodyEl.classList.add(Theme.LIGHT);
+
 inputEl.addEventListener('change', onSelectTheme);
 
 function onSelectTheme(evt) {
-    
+  evt.preventDefault();
+  bodyEl.classList.add(Theme.LIGHT);
+  bodyEl.classList.toggle(Theme.DARK);
+  
+  if (bodyEl.classList.value === 'light-theme') {
+    localStorage.setItem('theme', Theme.LIGHT);
+  } else {
+    localStorage.setItem('theme', Theme.DARK);
+  }
 }
 
+ function reload() {
+    const saveTheme = localStorage.getItem('theme');
+   if (saveTheme) {
+     console.log('saveTheme :>> ', saveTheme);
+     if (saveTheme === 'dark-theme') {
+       inputEl.checked = true;
+       bodyEl.classList.add(Theme.DARK);
+       localStorage.setItem('theme', Theme.DARK);
+     } else {
+       inputEl.checked = false;
+       bodyEl.classList.add(Theme.LIGHT);
+       localStorage.setItem('theme', Theme.LIGHT);
+     }
+    }
+  };
